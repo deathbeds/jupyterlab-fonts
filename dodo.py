bdb0c2868ecc03375f08decf8c0956e15e2ce53b
Nicholas Bollweg <nick.bollweg@gmail.com>
2021-08-03 22:00:18 -0400

    more lint

--- dodo.py
+++ dodo.py
@@ -3,28 +3,32 @@ from pathlib import Path
 
 
 def task_binder():
-    yield dict(
-        name="all",
-        actions=[["echo", "ok"]]
-    )
+    yield dict(name="all", actions=[["echo", "ok"]])
 
 
 def task_lint():
-    yield dict(
-        name="black",
-        actions=[["black", *P.ALL_PY]],
-        file_dep=[*P.ALL_PY]
-    )
+    yield dict(name="black", actions=[["black", *P.ALL_PY]], file_dep=[*P.ALL_PY])
 
     yield dict(
         name="flake8",
         actions=[["flake8", *P.ALL_PY]],
         task_dep=["lint:black"],
-        file_dep=[*P.ALL_PY]
+        file_dep=[*P.ALL_PY],
     )
 
 
+class C:
+    """constants"""
+
+    JLPM = ["jlpm"]
+    PY = ["python"]
+    PYM = [*PY, "-m"]
+    PIP = [*PYM, "pip"]
+
+
 class P:
+    """paths"""
+
     DODO = Path(__file__)
     ROOT = DODO.parent
     ALL_PY = [*ROOT.glob("*.py")]
