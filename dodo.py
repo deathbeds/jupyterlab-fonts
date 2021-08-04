fa6ab07cfb73622b8d45f17c7bb67f140c1cf374
Nicholas Bollweg <nick.bollweg@gmail.com>
2021-08-03 21:56:47 -0400

    start doit

---
+++ dodo.py
@@ -0,0 +1,38 @@
+"""project automation for jupyterlab-fonts"""
+from pathlib import Path
+
+
+def task_binder():
+    yield dict(
+        name="all",
+        actions=[["echo", "ok"]]
+    )
+
+
+def task_lint():
+    yield dict(
+        name="black",
+        actions=[["black", *P.ALL_PY]],
+        file_dep=[*P.ALL_PY]
+    )
+
+    yield dict(
+        name="flake8",
+        actions=[["flake8", *P.ALL_PY]],
+        task_dep=["lint:black"],
+        file_dep=[*P.ALL_PY]
+    )
+
+
+class P:
+    DODO = Path(__file__)
+    ROOT = DODO.parent
+    ALL_PY = [*ROOT.glob("*.py")]
+
+
+DOIT_CONFIG = {
+    "backend": "sqlite3",
+    "verbosity": 2,
+    "par_type": "thread",
+    "default_tasks": ["binder"],
+}
